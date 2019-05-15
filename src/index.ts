260f1e709f998a8be472ed0cb708c7eec2108358
Nicheal <nichealpham@gmail.com>
2019-05-15 22:51:37 +0700

    Migrate modules/google-storage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { SandraCore } from "./modules/server";
 export { IServerRoute as ISandraCoreRoute } from "./modules/server";
 export { IServerConfig as ISandraCoreConfig } from "./modules/server";
-export { GoogleStorage as SandraStorage } from "./goole-storage";
-export { IGoogleStorageConfig as ISandraStorageConfig } from "./goole-storage";
+export { GoogleStorage as SandraStorage } from "./modules/goole-storage";
+export { IGoogleStorageConfig as ISandraStorageConfig } from "./modules/goole-storage";
 export { MongoDb as SandraMongo } from "./modules/mongodb";
 export { IMongoDbConfig as ISandraMongoConfig } from "./modules/mongodb";
 export { MongoUltility as SandraMongoFunction } from './modules/mongodb';
