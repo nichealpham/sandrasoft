d921a2e256f4e6a1c6367f914cbe40fc06314c47
Nicheal <nichealpham@gmail.com>
2019-05-27 08:54:27 +0700

    Add modules/helper/index

---
+++ dest/modules/helper/index.d.ts
@@ -0,0 +1,4 @@
+export * from './services/date_helper';
+export * from './services/file_helper';
+export * from './services/system_helper';
+export * from './services/terminal_helper';
--- dest/modules/helper/index.js
+++ dest/modules/helper/index.js
@@ -1,1 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./services/date_helper"));
+__export(require("./services/file_helper"));
+__export(require("./services/system_helper"));
+__export(require("./services/terminal_helper"));
 //# sourceMappingURL=index.js.map
